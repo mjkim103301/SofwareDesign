c9095733b411aa2c0343071e52a759795f67cc8a
mjkim <mjkim103301@gmail.com>
2020-05-02 23:45:50 +0900

    애플릿 뷰어 오류 발생..

---
+++ src/Chap08_TemplateMethodPattern/MyApplet.java
@@ -0,0 +1,30 @@
+package Chap08_TemplateMethodPattern;
+
+import javax.swing.*;
+import java.applet.Applet;
+import java.awt.*;
+
+public class MyApplet extends Applet {
+    String message;
+    public void init(){
+        message="안녕하세요. 제가 왔어요...";
+        repaint();
+    }
+    public void start(){
+        message="시작중...";
+        repaint();
+    }
+    public void stop(){
+        message="저를 떠나려 하시는군요...";
+        repaint();
+    }
+    public void destroy(){
+        message="애플릿 제거됨";
+        repaint();
+    }
+    public void paint(Graphics g){
+        g.drawString(message, 5, 15);
+    }
+    
+
+}
--- src/Chap08_TemplateMethodPattern/MyFrame.java
+++ src/Chap08_TemplateMethodPattern/MyFrame.java
@@ -1,7 +1,23 @@
 package Chap08_TemplateMethodPattern;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MyFrame extends JFrame {
+    public MyFrame(String title){
+        super(title);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(300,300);
+        this.setVisible(true);
+    }
+    public void paint(Graphics graphics){//후크 메소드
+        super.paint(graphics);
+        String msg="내가 최고!!";
+        graphics.drawString(msg, 100,100);
+    }
+    public static void main(String [] args){
+        MyFrame myFrame=new MyFrame("헤드 퍼스트 디자인 패턴");
+
+    }
 
 }
