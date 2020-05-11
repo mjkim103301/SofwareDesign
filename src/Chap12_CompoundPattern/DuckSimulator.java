2711d47fa9c55ceaf51fb52bd01942d2763fe90e
mjkim <mjkim103301@gmail.com>
2020-05-11 17:21:15 +0900

    Compound Pattern version3
    with Decorator pattern

--- src/Chap12_CompoundPattern/DuckSimulator.java
+++ src/Chap12_CompoundPattern/DuckSimulator.java
@@ -1,23 +1,27 @@
 package Chap12_CompoundPattern;
 
+import Chap08_TemplateMethodPattern.Sort.Duck;
+
 public class DuckSimulator {
     public static void main(String[] args){
         DuckSimulator simulator=new DuckSimulator();
         simulator.simulate();
     }
     void simulate(){
-        Quackable mallarDuck=new MallardDuck();
-        Quackable redheadDuck=new RedheadDuck();
-        Quackable duckCall=new DuckCall();
-        Quackable rubberDuck=new RubberDuck();
+        Quackable mallarDuck=new QuackCounter(new MallardDuck());
+        Quackable redheadDuck=new QuackCounter(new RedheadDuck());
+        Quackable duckCall=new QuackCounter(new DuckCall());
+        Quackable rubberDuck=new QuackCounter(new RubberDuck());
         Quackable gooseDuck=new GooseAdaptor(new Goose());
 
-        System.out.println("\n Duck Simulator ");
+        System.out.println("\n Duck Simulator : With Decorator ");
         simulate(mallarDuck);
         simulate(redheadDuck);
         simulate(duckCall);
         simulate(rubberDuck);
         simulate(gooseDuck);
+
+        System.out.println("The ducks quacked "+QuackCounter.getQuacks()+" times");
     }
 
     private void simulate(Quackable duck) {
